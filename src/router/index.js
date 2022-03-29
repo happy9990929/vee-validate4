ac2a4577036543c15436d211d56beb0320710e3e
happy9990929 <lala9990929@gmail.com>
2022-03-29 11:47:17 +0800

    fix(home.vue): home => index

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import(/* webpackChunkName: "Home" */ '../views/Home.vue')
+    name: 'Index',
+    component: () => import(/* webpackChunkName: "Index" */ '../views/Index.vue')
   },
 ]
 
